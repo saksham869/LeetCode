7fa01c877cb307cfa003c7962dddc72e901c9c01
satyam mishra <mishrasatyam3456@gmail.com>
2025-04-21 17:44:28 +0530

    Time: 10 ms (80.29%), Space: 48.9 MB (24.34%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g); // Sort greed array
+        Arrays.sort(s); // Sort cookie sizes
+
+        int i = 0; // Pointer for children
+        int j = 0; // Pointer for cookies
+
+        while (i < g.length && j < s.length) {
+            if (s[j] >= g[i]) {
+                // Cookie is enough for child
+                i++; // Move to next child
+            }
+            j++; // Try next cookie
+        }
+
+        return i; // Number of satisfied children
+    }
+}
