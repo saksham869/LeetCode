11a8a20d0ff8c8ebadb15dad272a79093333f4e2
satyam mishra <mishrasatyam3456@gmail.com>
2025-04-21 18:09:00 +0530

    Time: 11 ms (39.13%), Space: 48.7 MB (45.6%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -7,10 +7,14 @@ class Solution {
 
         int i = 0; // Pointer for children
         int j = 0; // Pointer for cookies
+        int count=0;
+
 
         while (i < g.length && j < s.length) {
             if (s[j] >= g[i]) {
                 // Cookie is enough for child
+                count++;
+
                 i++; // Move to next child
             }
             j++; // Try next cookie
