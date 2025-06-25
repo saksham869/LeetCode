7878ae9ab95ac0f02e3607a13bce316459acde3f
satyam mishra <mishrasatyam3456@gmail.com>
2025-06-25 10:13:27 +0530

    Time: 1 ms (87.18%), Space: 40.9 MB (69.63%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,28 +1,22 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x == 0 || x == 1) return x; // Edge cases
+        if (x < 2) return x;
 
-        int left = 1, right = x, ans = 0;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            long square = (long) mid * mid; // Use long to prevent overflow
+        int low = 1, high = x / 2, ans = 1;
 
-            if (square == x) return mid; // Perfect square
-            else if (square < x) {
-                ans = mid; // Store possible answer
-                left = mid + 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long sq = (long) mid * mid;
+
+            if (sq == x) return mid;
+            else if (sq < x) {
+                ans = mid;
+                low = mid + 1;
             } else {
-                right = mid - 1;
+                high = mid - 1;
             }
         }
-        return ans; // The floor value of sqrt(x)
-    }
 
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.mySqrt(4));   // Output: 2
-        System.out.println(solution.mySqrt(8));   // Output: 2
-        System.out.println(solution.mySqrt(16));  // Output: 4
-        System.out.println(solution.mySqrt(2147395599)); // Large input test
+        return ans;
     }
 }
