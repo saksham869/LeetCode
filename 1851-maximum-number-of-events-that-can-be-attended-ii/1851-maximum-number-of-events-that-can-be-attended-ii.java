da2153726a6a415dca503705214c732e39134058
satyam mishra <mishrasatyam3456@gmail.com>
2025-07-08 21:32:19 +0530

    Time: 67 ms (61.54%), Space: 121 MB (97.6%) - LeetHub

--- 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.java
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.java
@@ -3,50 +3,48 @@ import java.util.*;
 class Solution {
     public int maxValue(int[][] events, int k) {
         int n = events.length;
-        // Sort events by start time
-        Arrays.sort(events, Comparator.comparingInt(a -> a[0]));
+        Arrays.sort(events, (a, b) -> a[1] - b[1]); // sort by end time
         
-        // Prepare array of start times for binary search
-        int[] starts = new int[n];
+        // Create an array of end times for binary search
+        int[] ends = new int[n];
         for (int i = 0; i < n; i++) {
-            starts[i] = events[i][0];
+            ends[i] = events[i][1];
         }
-        
-        // DP memo: dp[i][j] = max value from event i with j events left
-        Integer[][] memo = new Integer[n][k + 1];
-        
-        return dfs(0, k, events, starts, memo);
+
+        // dp[i][j] = max value using first i events and attending j events
+        int[][] dp = new int[n + 1][k + 1];
+
+        for (int i = 1; i <= n; i++) {
+            int[] curr = events[i - 1];
+            int start = curr[0], end = curr[1], value = curr[2];
+
+            // Find last event that doesn't overlap
+            int last = binarySearch(ends, start - 1);
+
+            for (int j = 1; j <= k; j++) {
+                // Option 1: Don't take current event
+                dp[i][j] = Math.max(dp[i][j], dp[i - 1][j]);
+
+                // Option 2: Take current event
+                dp[i][j] = Math.max(dp[i][j], dp[last + 1][j - 1] + value);
+            }
+        }
+
+        return dp[n][k];
     }
-    
-    private int dfs(int idx, int rem, int[][] events, int[] starts, Integer[][] memo) {
-        if (idx >= events.length || rem == 0) return 0;
-        if (memo[idx][rem] != null) return memo[idx][rem];
-        
-        // Option 1: skip current event
-        int ans = dfs(idx + 1, rem, events, starts, memo);
-        
-        // Option 2: attend current event
-        int end = events[idx][1];
-        int value = events[idx][2];
-        
-        // Find next event whose start >= current end + 1
-        int nextIdx = lowerBound(starts, end + 1);
-        
-        int take = value + dfs(nextIdx, rem - 1, events, starts, memo);
-        
-        ans = Math.max(ans, take);
-        memo[idx][rem] = ans;
+
+    // Binary search: Find the rightmost event that ends before or at 'time'
+    private int binarySearch(int[] ends, int time) {
+        int low = 0, high = ends.length - 1, ans = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (ends[mid] <= time) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
         return ans;
-    }
-    
-    // Standard binary search for first starts[pos] >= target
-    private int lowerBound(int[] starts, int target) {
-        int lo = 0, hi = starts.length;
-        while (lo < hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (starts[mid] >= target) hi = mid;
-            else lo = mid + 1;
-        }
-        return lo;
     }
 }
