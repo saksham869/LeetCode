22b67628a798911e9d7e06622bb609b4d83ac0d5
satyam mishra <mishrasatyam3456@gmail.com>
2025-07-27 14:26:19 +0530

    Time: 1 ms (82.92%), Space: 41.9 MB (91.01%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -2,14 +2,13 @@ import java.util.*;
 
 public class Solution {
 
-    // Function to generate Pascal's Triangle
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> triangle = new ArrayList<>();
 
         for (int i = 0; i < numRows; i++) {
             List<Integer> row = new ArrayList<>();
             for (int j = 0; j <= i; j++) {
-                if (j == 0 || j == i) row.add(1); // First and last element are always 1
+                if (j == 0 || j == i) row.add(1);
                 else row.add(triangle.get(i - 1).get(j - 1) + triangle.get(i - 1).get(j));
             }
             triangle.add(row);
@@ -18,13 +17,9 @@ public class Solution {
         return triangle;
     }
 
-    // Main method to run the program
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int numRows = 5; // You can change this to test with other values
-        List<List<Integer>> result = sol.generate(numRows);
-
-        // Print Pascal's Triangle
+        List<List<Integer>> result = sol.generate(5);
         for (List<Integer> row : result) {
             System.out.println(row);
         }
