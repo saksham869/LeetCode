8a8ec1bf420db5d1141fc88f4b4a756dc79c0139
satyam mishra <mishrasatyam3456@gmail.com>
2025-08-14 11:35:41 +0530

    Time: 0 ms (100%), Space: 45.7 MB (60.82%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,4 +1,4 @@
-public class Solution {
+/*public class Solution {
     public int search(int[] nums, int target) {
         int low = 0;
         int high = nums.length - 1;
@@ -15,6 +15,29 @@ public class Solution {
             }
         }
 
-        return -1; // not found
+        return -1; 
+
+    }
+}*/
+public class Solution {
+    public int search(int[] nums, int target) {
+        return binarySearch(nums, target, 0, nums.length - 1);
+    }
+
+    private int binarySearch(int[] nums, int target, int low, int high) {
+        if (low > high) {
+            return -1; // Base case: not found
+        }
+
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] == target) {
+            return mid; // Target found
+        } else if (nums[mid] < target) {
+            return binarySearch(nums, target, mid + 1, high); // Search right half
+        } else {
+            return binarySearch(nums, target, low, mid - 1); // Search left half
+        }
     }
 }
+
