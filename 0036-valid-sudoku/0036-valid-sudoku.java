3fc8d5b55ffb99b67066158cd424aaa58fa1fd42
satyam mishra <mishrasatyam3456@gmail.com>
2025-08-15 14:24:37 +0530

    Time: 14 ms (15.35%), Space: 44.8 MB (32.2%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,44 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> seen = new HashSet<>();
+
+        // Iterate through the 9x9 board
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char num = board[i][j];
+
+                if (num != '.') {  // Ignore empty cells
+                    // Generate unique keys for row, column, and 3x3 box
+                    String rowKey = num + " in row " + i;
+                    String colKey = num + " in col " + j;
+                    String boxKey = num + " in box " + (i / 3) + "-" + (j / 3);
+
+                    // If any key already exists, the board is invalid
+                    if (!seen.add(rowKey) || !seen.add(colKey) || !seen.add(boxKey)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true; // If no conflicts, the board is valid
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        char[][] board = {
+            {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+            {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+            {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+            {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+            {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+            {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+            {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+            {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+            {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+
+        System.out.println(sol.isValidSudoku(board)); // Output: true
+    }
+}
