54acb2195cbad034be9680e60fb3a4333c147fa5
satyam mishra <mishrasatyam3456@gmail.com>
2025-10-30 21:58:56 +0530

    Time: 670 ms (74.34%), Space: 0B (100%) - LeetHub

---
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -0,0 +1,16 @@
+WITH first_year_per_product AS (
+  SELECT
+    product_id,
+    MIN(year) AS first_year
+  FROM Sales
+  GROUP BY product_id
+)
+SELECT
+  s.product_id,
+  f.first_year,
+  s.quantity,
+  s.price
+FROM Sales s
+JOIN first_year_per_product f
+  ON s.product_id = f.product_id
+ AND s.year = f.first_year;
