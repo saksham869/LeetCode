3675fcd27db8c57f9845803e3c60543873a9b99b
satyam mishra <mishrasatyam3456@gmail.com>
2025-10-30 21:38:51 +0530

    Time: 231 ms (66.41%), Space: 0B (100%) - LeetHub

---
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -0,0 +1,9 @@
+SELECT machine_id, ROUND(AVG(end - start), 3) AS processing_time
+FROM (
+  SELECT machine_id, process_id,
+         MAX(CASE WHEN activity_type = 'start' THEN timestamp END) AS start,
+         MAX(CASE WHEN activity_type = 'end' THEN timestamp END) AS end
+  FROM Activity
+  GROUP BY machine_id, process_id
+) subq
+GROUP BY machine_id;
