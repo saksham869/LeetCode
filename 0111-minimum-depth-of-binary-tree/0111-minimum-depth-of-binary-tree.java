23be3773ce7bab4d07f3ee68f53b0762aff98041
satyam mishra <mishrasatyam3456@gmail.com>
2026-01-24 11:54:47 +0530

    Time: 5 ms (26.13%), Space: 82.1 MB (62.09%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -1,28 +1,81 @@
-import java.util.LinkedList;
-import java.util.Queue;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 
-public class Solution {
-    public int minDepth(TreeNode root) {
-        if (root == null) return 0;
+// class Solution {
+//     public int minDepth(TreeNode root) {
+//         if (root == null) return 0;
 
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        int depth = 1;
+//         Queue<TreeNode> queue = new LinkedList<>();
+//         queue.add(root);
 
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                // Check if it's a leaf node
-                if (node.left == null && node.right == null) {
-                    return depth;
-                }
-                // Enqueue children
-                if (node.left != null) queue.offer(node.left);
-                if (node.right != null) queue.offer(node.right);
-            }
-            depth++;
+//         int depth = 1;
+
+//         while (!queue.isEmpty()) {
+//             int size = queue.size();
+
+//             for (int i = 0; i < size; i++) {
+//                 TreeNode node = queue.poll();
+
+//                 if (node.left == null && node.right == null) {
+//                     return depth;
+//                 }
+
+//                 if (node.left != null) queue.add(node.left);
+//                 if (node.right != null) queue.add(node.right);
+//             }
+
+//             depth++;
+//         }
+
+//         return depth;
+//     }
+// }
+class Solution{
+    public int minDepth(TreeNode root){
+        if(root == null){
+            return 0;
+
         }
-        return depth;
+        if(root.left == null && root.right == null){
+            return 1;
+
+        }
+        if(root.left == null){
+            return 1+ minDepth(root.right);
+        }
+        if(root.right == null){
+            return 1+ minDepth(root.left);
+        }
+        return 1+ Math.min(minDepth(root.left),minDepth(root.right));
+
+        
     }
 }
