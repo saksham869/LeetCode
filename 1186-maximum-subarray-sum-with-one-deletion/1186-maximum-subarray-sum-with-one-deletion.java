26e3862dbd0ef850fdef84730814962ce9956d4e
satyam mishra <mishrasatyam3456@gmail.com>
2026-03-23 11:09:09 +0530

    Time: 6 ms (91.63%), Space: 55.9 MB (76.27%) - LeetHub

---
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maximumSum(int[] arr) {
+        int dp0 = arr[0];
+        int dp1 = 0;
+        int result = arr[0];
+
+        for(int i = 1; i<arr.length; i++){
+            dp1 = Math.max(dp0 , dp1 + arr[i]);
+            dp0 = Math.max(arr[i], dp0+arr[i]);
+
+            result = Math.max(result,Math.max(dp0 , dp1));
+        }
+        return result;
+
+        
+    }
+}
